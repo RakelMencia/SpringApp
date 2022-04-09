d7a3da9e0112e7aca344b4981e6dd953a21e60d5
RaquelMC <raquel.menciac@gmail.com>
2022-04-09 20:05:29 +0200

    09/04: Se añade el cálculo de la desviación típica.

--- SpringApp/src/main/java/com/raquelmc/springApp/Helper.java
+++ SpringApp/src/main/java/com/raquelmc/springApp/Helper.java
@@ -1,16 +1,11 @@
 package com.raquelmc.springApp;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
@@ -41,6 +36,7 @@ public class Helper {
 			statistict.setMean(getMean(doubleListOfValues));
 			statistict.setMedian(getMedian(doubleListOfValues));
 			statistict.setMode(getMode(doubleListOfValues));
+			statistict.setStandardDesviation(getStandardDesviation(doubleListOfValues, statistict.getMean()));
 		}
 		return statistict;
 	}
@@ -76,39 +72,54 @@ public class Helper {
 	}
 
 	private double getMedian(List<Double> doubleValues) {
-		List<Double> sortedList = doubleValues.stream().sorted().collect(Collectors.toList());	
-		return sortedList.size() % 2 == 0 ? (sortedList.get((sortedList.size() / 2) - 1) + sortedList.get(sortedList.size() / 2)) / 2 :
-			sortedList.get(sortedList.size() / 2);
+		List<Double> sortedList = doubleValues.stream().sorted().collect(Collectors.toList());
+		return sortedList.size() % 2 == 0
+				? (sortedList.get((sortedList.size() / 2) - 1) + sortedList.get(sortedList.size() / 2)) / 2
+				: sortedList.get(sortedList.size() / 2);
 	}
 
 	private List<Double> getMode(List<Double> doubleValues) {
 		List<Double> modeValues = new ArrayList<Double>();
-	    Map<Double,Integer> hmMode = new HashMap<Double,Integer>();
-	    //Guardamos en un map el valor y las repeticiones que tiene
-	    int max  = 1;
-	    for(int i = 0; i < doubleValues.size(); i++) {
-	        if (hmMode.get(doubleValues.get(i)) != null) {
-	            int count = hmMode.get(doubleValues.get(i));
-	            count++;
-	            hmMode.put(doubleValues.get(i), count);
-	            if(count > max) max  = count;                
-	        } else { 
-	        	hmMode.put(doubleValues.get(i),1);
-	        }
-	    }
-	    //Ordenamos el map por sus repeticiones
-        Map<Double,Integer> sortedMap = hmMode.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(
-        		Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new
-        ));
-        //Obtenemos el valor/valores que más veces se repiten, por si hubiese más de una moda        
-        int maxRepetitions = sortedMap.entrySet().stream().max((entry1, entry2) -> Integer.compare(entry1.getValue(), entry2.getValue())).get().getValue();
-        for(Map.Entry<Double,Integer> entry : sortedMap.entrySet()) {
-        	if(entry.getValue()==maxRepetitions) modeValues.add(entry.getKey());       		
-        }        
-	    return modeValues;
+		Map<Double, Integer> hmMode = new HashMap<Double, Integer>();
+		// Guardamos en un map el valor y las repeticiones que tiene
+		int max = 1;
+		for (int i = 0; i < doubleValues.size(); i++) {
+			if (hmMode.get(doubleValues.get(i)) != null) {
+				int count = hmMode.get(doubleValues.get(i));
+				count++;
+				hmMode.put(doubleValues.get(i), count);
+				if (count > max)
+					max = count;
+			} else {
+				hmMode.put(doubleValues.get(i), 1);
+			}
+		}
+		// Ordenamos el map por sus repeticiones
+		Map<Double, Integer> sortedMap = hmMode.entrySet().stream().sorted(Map.Entry.comparingByValue())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+		// Obtenemos el valor/valores que más veces se repiten, por si hubiese más de
+		// una moda
+		int maxRepetitions = sortedMap.entrySet().stream()
+				.max((entry1, entry2) -> Integer.compare(entry1.getValue(), entry2.getValue())).get().getValue();
+		for (Map.Entry<Double, Integer> entry : sortedMap.entrySet()) {
+			if (entry.getValue() == maxRepetitions)
+				modeValues.add(entry.getKey());
+		}
+		return modeValues;
 	}
-	
-	
+
+	private double getStandardDesviation(List<Double> doubleValues, double mean) {
+		double variance = 0;
+		// Primero calculamos la varianza a partir de los valores de la lista
+		for (double value : doubleValues) {
+			double range = 0;
+			range = Math.pow(value - mean, 2);
+			variance += range;// Sumamos el rango que hemos calculado de todos los valores para obtener la
+								// varianza
+		}
+		return Math.sqrt(variance / doubleValues.size());
+	}
+
 	public void save(Statistic document) {
 		repository.save(document);
 	}
